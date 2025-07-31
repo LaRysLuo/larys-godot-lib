b3b4df48b28219e8af760e24d28112898db14ca8
悠月菌 <55402305+LaRysLuo@users.noreply.github.com>
2025-07-31 20:48:44 +0800

    增加随机字符串的工具类

---
+++ src/utils/string_utils.gd
@@ -0,0 +1,14 @@
+extends Node
+class_name StringUtils
+
+## 随机生成4位字符串
+static func random_string(length: int = 4) -> String:
+    const charset := "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    var rng := RandomNumberGenerator.new()
+    rng.randomize()
+    
+    var result := ""
+    for i in length:
+        var idx = rng.randi_range(0, charset.length() - 1)
+        result += charset[idx]
+    return result
